b03c6c05b50493be3c5c6b41bbafa5b37f9e3a19
Anunay <anunay.anindya@tesco.com>
2018-10-26 12:33:59 +0530

    custom exception

--- emp-app/src/main/java/com/socgen/empapp/business/EmployeeService.java
+++ emp-app/src/main/java/com/socgen/empapp/business/EmployeeService.java
@@ -13,48 +13,42 @@ import com.socgen.empapp.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
-	
+
 	@Autowired
 	public EmployeeRepository employeeRepository;
-	
+
 	@Transactional
 	public Employee addEmployee(Employee employee) {
-		
+
 		return employeeRepository.save(employee);
 	}
-	
+
 	public List<Employee> getEmployeesFilteredByDept(String dept) {
-		
-		return ((List<Employee>) employeeRepository.findAll())
-				.stream()
-				.filter(employee -> {
-					return employee.getDept().equals(dept);
-				})
-				.collect(Collectors.toList());
+
+		return ((List<Employee>) employeeRepository.findAll()).stream().filter(employee -> {
+			return employee.getDept().equals(dept);
+		}).collect(Collectors.toList());
 	}
-	
+
 	public List<Employee> getEmployeesFilteredByDeptAndAge(String dept, Integer age) {
-		
-		return ((List<Employee>) employeeRepository.findAll())
-				.stream()
-				.filter(employee -> {
-					return employee.getDept().equals(dept) && employee.getAge() >= age;
-				})
-				.collect(Collectors.toList());
+
+		return ((List<Employee>) employeeRepository.findAll()).stream().filter(employee -> {
+			return employee.getDept().equals(dept) && employee.getAge() >= age;
+		}).collect(Collectors.toList());
 	}
 
 	@Transactional
 	public Employee deleteById(Integer id) {
-		
+
 		Employee emp = employeeRepository.findOne(id);
-			employeeRepository.delete(id);
+		employeeRepository.delete(id);
 		return emp;
 	}
 
 	public Employee getEmployeeById(Integer id) {
 		List<Employee> empList = employeeRepository.findById(id);
-		if(empList.isEmpty())
-			throw new EmpAppCustomException("Employee Not Found by id = " + id);
+		if (empList.isEmpty())
+			throw new EmpAppCustomException(EmployeeService.class, "Employee Not Found by id = ", id);
 		return employeeRepository.findById(id).get(0);
 	}
 
--- emp-app/src/main/java/com/socgen/empapp/common/EmpAppCustomException.java
+++ emp-app/src/main/java/com/socgen/empapp/common/EmpAppCustomException.java
@@ -1,17 +1,32 @@
 package com.socgen.empapp.common;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class EmpAppCustomException extends RuntimeException {
 
 	private static final long serialVersionUID = -7214458159477737441L;
-	
-	public EmpAppCustomException(String message) {
-		super(message);
+
+	public EmpAppCustomException(Class<?> clazz, String message, Object... paramsMap) {
+		super(generateMessage(clazz.getSimpleName(), message, toMap(String.class, String.class, paramsMap)));
 	}
-	
+
 	public EmpAppCustomException(String message, Throwable e) {
 		super(message, e);
 	}
-	
 
+	private static String generateMessage(String entity, String message, Map<String, String> params) {
+		return StringUtils.capitalize(entity) + message + params;
+	}
+
+	private static <K, V> Map<K, V> toMap(Class<K> keyType, Class<V> valueType, Object... entries) {
+		if (entries.length % 2 == 1)
+			throw new IllegalArgumentException("Invalid entries");
+		return IntStream.range(0, entries.length / 2).map(i -> i * 2).collect(HashMap::new,
+				(m, i) -> m.put(keyType.cast(entries[i]), valueType.cast(entries[i + 1])), Map::putAll);
+	}
 
 }
